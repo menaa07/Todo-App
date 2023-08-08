67bdd80b3837e21ba35bd91ebdb5f9f920d1934f
mena <stuf408478@ese.gov.ae>
2023-08-08 23:09:59 +0400

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Mena Haitham on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
